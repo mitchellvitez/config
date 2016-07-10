52da9f9e301afe66360856abee1094fc8d32db3f
Mitchell Vitez <mitchell@vitez.me>
2016-07-10 12:10:00 -0400

    Don't use brew if not on mac

--- config.sh
+++ config.sh
@@ -5,15 +5,18 @@
 mv config/* .
 rm -rf config/
 
-defaults write -g InitialKeyRepeat -int 10
-defaults write -g KeyRepeat -int 1
+if [ "$(uname)" == "Darwin" ]; then
+  defaults write -g InitialKeyRepeat -int 10
+  defaults write -g KeyRepeat -int 1
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install tmux
-brew install ag
-brew install macvim
+  brew install tmux
+  brew install ag
+  brew install macvim
+fi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
+
