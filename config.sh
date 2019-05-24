fba6257cefb63275838ec7aed4f62d6141e81e61
Mitchell Vitez <mitchell@vitez.me>
2019-05-24 14:05:06 -0700

    Add ubuntu steps

--- config.sh
+++ config.sh
@@ -4,11 +4,14 @@
 
 cd ~
 mv config/* .
+mv config/.* .
 rm -rf config/
 
+# mac
 if [ "$(uname)" == "Darwin" ]; then
   defaults write -g InitialKeyRepeat -int 10
   defaults write -g KeyRepeat -int 1
+  defaults write com.apple.finder AppleShowAllFiles YES
 
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -17,14 +20,20 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install macvim --with-cscope --with-lua
   brew linkapps macvim
   brew install fzf
+  /usr/local/opt/fzf/install
+fi
+# ubuntu
+if [ "$(uname)" == "Linux" ]; then
+  sudo apt-get install tmux
+  sudo apt-get install silversearcher-ag
+  sudo apt-get install vim-nox
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
 fi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/soyuka/tmux-current-pane-hostname ~/tmux
 
-/usr/local/opt/fzf/install
-
 vim +PluginInstall +qall
-
+rm -rf ~/.git/
 source ~/.bash_profile
-
